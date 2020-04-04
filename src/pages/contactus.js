ac258661b71a1f1d7ceabd50666a5356c3320d2d
Tony Lee <hy.tony.lee@gmail.com>
2020-04-04 11:16:36 -0700

    First commit

---
+++ src/pages/contactus.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const contactus = () => {
+  return (
+    <Layout>
+      <Link to="/">Home</Link>
+      <h1>Contact us</h1>
+    </Layout>
+  )
+}
+
+export default contactus
